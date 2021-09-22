7f614adb290f9d588be57f40d6c43834f9ada3a4
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-09-22 15:53:17 +0900

    linter追加

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -1,37 +1,26 @@
-import React from 'react';
+import { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const name = 'Patty';
-  const greet = (name: string) => <p>Hello, {name || 'Guest'}!</p>;
-  
-  const n = Math.floor(Math.random() * 10); // 0 〜 9 の整数を生成
-  const threshold = 5;
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>{greet(name)}</div>
-        <p>
-          Hello World
-        </p>
-        <div>
-          {n > threshold && <p>`n` is larger than {threshold}</p>}
-          <p>`n` is {n % 2 === 0 ? 'even' : 'odd'}</p>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.tsx</code>
+        and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
